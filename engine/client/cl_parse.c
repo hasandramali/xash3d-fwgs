40af4bb74cb5ce849f96cfcaf0c694bcd2f30949
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-01-16 12:09:49 +0200

    Update cl_parse.c

--- engine/client/cl_parse.c
+++ engine/client/cl_parse.c
@@ -2142,7 +2142,7 @@ Set screen fade
 */
 static void CL_ParseScreenFade( sizebuf_t *msg )
 {
-	if ( Cvar_VariableInteger( "cl_noscreenfade" ) ) return 1;
+	if ( !Q_stricmp ( cvarName, "cl_noscreenfade" ) ) return 1;
 	
 	float		duration, holdTime;
 	screenfade_t	*sf = &clgame.fade;
