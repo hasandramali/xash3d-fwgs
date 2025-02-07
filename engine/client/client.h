7e073d4c8d75df7192be7ad0d5640da76b37a29c
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-02-07 02:39:07 +0200

    Update client.h

--- engine/client/client.h
+++ engine/client/client.h
@@ -704,7 +704,7 @@ extern convar_t	m_ignore;
 extern convar_t	r_showtree;
 extern convar_t	ui_renderworld;
 extern convar_t cl_fixmodelinterpolationartifacts;
-extern convar_t cl_noscreenfade;
+extern convar_t cl_ticket_custom;
 
 //=============================================================================
 
