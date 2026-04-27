ffc0b03970443053a13cc2b89f9907ad3a8998a6
hasandramali <hasandramali09@gmail.com>
2026-04-28 01:28:56 +0300

    skzsx

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -234,6 +234,10 @@ typedef struct
 	uint max_entities;
 
 	ref_screen_rotation_t rotation;
+
+	// display scaling for resolution changes (Android)
+	float scale_x;
+	float scale_y;
 } gl_globals_t;
 
 typedef struct
