6f3fe3294aebc99ca1ff1866f8605bd05e77f04f
hasandramali <hasandramali09@gmail.com>
2026-06-14 23:16:18 +0300

    fix: resolve libbacktrace config.h missing error   and exclude elf.c on iOS

---
+++ ios/libbacktrace_config/config.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BACKTRACE_ELF_SIZE 64
+#define BACKTRACE_XCOFF_SIZE 64
+#define HAVE_ATOMIC_FUNCTIONS 1
+#define HAVE_SYNC_FUNCTIONS 1
+#define HAVE_DECL_GETPAGESIZE 1
+#define HAVE_DECL_STRNLEN 1
+#define HAVE_DLFCN_H 1
+#define HAVE_FCNTL 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LSTAT 1
+#define HAVE_MACH_O_DYLD_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_READLINK 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_MMAN_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_NAME "libbacktrace"
+#define PACKAGE_STRING "libbacktrace 1.0"
+#define PACKAGE_TARNAME "libbacktrace"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "1.0"
+
+#endif
