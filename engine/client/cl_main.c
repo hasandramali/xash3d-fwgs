ec298e78a877bbc40d6540ad1b93f5e46e9ea337
hasandramali <hasandramali09@gmail.com>
2026-09-25 16:28:58 +0300

    build

--- engine/client/cl_main.c
+++ engine/client/cl_main.c
@@ -43,7 +43,7 @@ static CVAR_DEFINE( cl_logoupdate, "@cl_logoupdate", "0", 0, "set by menu to tri
 CVAR_DEFINE_AUTO( cl_logomaxdim, "96", FCVAR_ARCHIVE, "maximum decal dimension" );
 static CVAR_DEFINE_AUTO( cl_test_bandwidth, "1", FCVAR_ARCHIVE, "test network bandwith before connection" );
 CVAR_DEFINE_AUTO( fps_max, "61", 0, "fps limit" );
-CVAR_DEFINE_AUTO( cl_fpsfilter, "0", FCVAR_ARCHIVE, "FPS filter mode: 0=use fps_max, 1=use max_fps, 2=use max_fps+fake msec" );
+CVAR_DEFINE_AUTO( cl_fpsfilter, "0", FCVAR_READ_ONLY, "FPS filter mode: 0=use fps_max, 1=use max_fps, 2=use max_fps+fake msec" );
 static CVAR_DEFINE_AUTO( cl_require_challenge_echo, "-1", FCVAR_ARCHIVE, "reject connect packets that don't echo challenge, protects against spoofed servers but breaks connection to old servers (-1 = engine default)" );
 
 CVAR_DEFINE_AUTO( fps_rate, "100", FCVAR_ARCHIVE, "fake FPS rate when cl_fpsfilter is 2" );
